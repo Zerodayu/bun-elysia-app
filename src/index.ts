1a3f57c445cad0096a5a5e86b10a126bc6e4dc58
Zerodayu <kimianalconera39@gmail.com>
2026-03-11 00:53:38 +0800

    update: server start

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Elysia } from "elysia";
+import { openapi } from "@elysiajs/openapi";
+import { apiRoutes } from "./routes";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(openapi())
+  .get("/", () => "Hello Elysia")
+  .group("/api/v1", (app) => app.use(apiRoutes))
+  .listen(8080);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
