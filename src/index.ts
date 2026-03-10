34208edb3d8a7dd1296f3c64d7ba3b00475cc32a
Zerodayu <kimianalconera39@gmail.com>
2026-03-11 01:45:49 +0800

    update: openApi documentation improve readability

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,18 @@ import { openapi } from "@elysiajs/openapi";
 import { apiRoutes } from "./routes";
 
 const app = new Elysia()
-  .use(openapi())
+  .use(
+    openapi({
+      documentation: {
+        info: {
+          version: "v1",
+          title: "Bun with Elysia backend",
+          description:
+            "Using Bun and Elysia, Developing a fast and type safe scalable backend",
+        },
+      },
+    }),
+  )
   .get("/", () => "Hello Elysia", {
     detail: {
       hide: true,
